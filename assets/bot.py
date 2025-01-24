736787a76df508263ec7b9dfda9d4def4faef3cc
Aleksandr <sanek00000007@gmail.com>
2025-01-24 12:07:31 +0300

    Added preprocessing for items

--- assets/bot.py
+++ assets/bot.py
@@ -15,7 +15,8 @@ class SteamBot:
 
             item_url = "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Slate%20(Field-Tested)"
             json_data = self.parser.get_json_items_from_market(item_url)
-            pprint(json_data)
+            # pprint(json_data)
             items = self.parser.extract_item_data(json_data)
+            pprint(items)
 
         # Основная логика бота
--- assets/parser.py
+++ assets/parser.py
@@ -10,9 +10,8 @@ class Parser:
         self.steam_session: SteamSession = session
         self.currency: Currency = currency
 
-    def get_json_items_from_market(self, url):
+    def get_json_items_from_market(self, url: str) -> dict:
         response = self.steam_session.session.get(url)
-
         if response.status_code != 200:
             raise Exception(
                 f"Response complete with code error: {response.status_code}"
@@ -20,15 +19,34 @@ class Parser:
         soup = BeautifulSoup(response.text, "lxml")
         items_table = soup.findAll("script", {"type": "text/javascript"})
         items = str(items_table[-1]).split("var g_rgListingInfo = ")[1].split(";")[0]
-        items_to_json = json.loads(items)
-        return items_to_json
+        return json.loads(items)
 
-    def extract_item_data(self, items_json):
+    def calculate_price(self, item_data: dict) -> float:
+        price_no_fee = int(item_data.get("price", 0))
+        fee = int(item_data.get("fee", 0))
+        currency_id = item_data.get("currencyid")
+        if currency_id is None:
+            raise ValueError("Missing currency_id in item data")
+        price = (price_no_fee + fee) / 100
+        return self.currency.change_currency(price, currency_id)
+
+    def construct_inspect_link(self, item_data: dict, listing_id: str) -> str:
+        raw_inspect_link = item_data.get("asset").get("market_actions")[0].get("link")
+        asset_id = item_data.get("asset").get("id")
+        return raw_inspect_link.replace("listingid", listing_id).replace(
+            "assetid", asset_id
+        )
+
+    def extract_item_data(self, items_json: dict) -> list[dict]:
         extracted_items = []
         for listing_id, item_data in items_json.items():
-            item_name = item_data.get("name", "Unknown")
-            price = item_data.get("price", "Unknown")
+            inspect_link = self.construct_inspect_link(item_data, listing_id)
+            price = self.calculate_price(item_data)
             extracted_items.append(
-                {"listing_id": listing_id, "name": item_name, "price": price}
+                {
+                    "listing_id": listing_id,
+                    "inspect_link": inspect_link,
+                    "price": price,
+                }
             )
         return extracted_items
