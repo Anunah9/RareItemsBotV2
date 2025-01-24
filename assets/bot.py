c11875e32043c9e855b3a60c3e26f54ff238b7b0
Aleksandr <sanek00000007@gmail.com>
2025-01-24 10:32:27 +0300

    Before session changes

--- assets/bot.py
+++ assets/bot.py
@@ -1,5 +1,6 @@
 from assets.parser import Parser
 from assets.session import ISteamSession
+from pprint import pprint
 
 
 class SteamBot:
@@ -11,6 +12,8 @@ class SteamBot:
             print("Bot started with an active session.")
             parser = Parser(self.session)
             item_url = "https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Slate%20(Field-Tested)"
-            items = parser.get_items_from_market(item_url)
+            json_data = parser.get_json_items_from_market(item_url)
+            pprint(json_data)
+            items = parser.extract_item_data(json_data)
 
         # Основная логика бота
--- assets/parser.py
+++ assets/parser.py
@@ -2,22 +2,35 @@ import re
 from bs4 import BeautifulSoup
 import requests
 from assets.session import SteamSession
+import json
+from pprint import pprint
 
 
 class Parser:
     def __init__(self, session: SteamSession):
         self.steam_session: SteamSession = session
 
-    def get_items_from_market(self, url):
+    def get_json_items_from_market(self, url):
         response = self.steam_session.session.get(url)
 
         if response.status_code != 200:
             raise Exception(
                 f"Response complete with code error: {response.status_code}"
             )
-
-        soup = BeautifulSoup(response.text)
-        items_table = soup.find("div", {"id": "searchResultsRows"})
-        items = items_table.find_all("div", id=re.compile(r"listing_\d+"))
-        print(items)
-        print(len(items))
+        soup = BeautifulSoup(response.text, "lxml")
+        items_table = soup.findAll("script", {"type": "text/javascript"})
+        items = str(items_table[-1]).split("var g_rgListingInfo = ")[1].split(";")[0]
+        items_to_json = json.loads(items)
+        return items_to_json
+    
+    def extract_item_data(self, items_json):
+        extracted_items = []
+        for listing_id, item_data in items_json.items():
+            item_name = item_data.get("name", "Unknown")
+            price = item_data.get("price", "Unknown")
+            extracted_items.append({
+                "listing_id": listing_id,
+                "name": item_name,
+                "price": price
+            })
+        return extracted_items
