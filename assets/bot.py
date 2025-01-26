18ba5bbfa4aee039beadd53b7642bf897ce801f8
Hanuna <burkovaa@stud.kai.ru>
2025-01-26 11:59:29 +0300

    -Renamed func get_item_info

--- assets/bot.py
+++ assets/bot.py
@@ -1,11 +1,54 @@
 from dataclasses import dataclass
+from typing import Protocol
 from assets.parser import Parser
 from assets.session import ISteamSession
 from assets.item import ItemData
 from pprint import pprint
-from assets.inspect import MockItemInfoFetcher
-from assets.prices import MockItemPriceFetcher
+from assets.inspect import IItemInfoFetcher, MockItemInfoFetcher
+from assets.prices import IItemPriceFetcher, MockItemPriceFetcher
 from assets.utils import create_message
+
+
+class ISteamBot(Protocol):
+    def __init__(
+        self,
+        session: ISteamSession,
+        parser: Parser,
+        itemInfoFetcher: IItemInfoFetcher,
+        itemPriceFetcher: IItemPriceFetcher,
+    ):
+        pass
+
+    def start(self):
+        pass
+
+    def get_items_from_market(self, item_url):
+        pass
+
+    def process_items(self, item_name, items):
+        pass
+
+    def get_sticker_and_charm_info(self, inspect_link):
+        pass
+
+    def extract_sticker_info(self, item_info):
+        pass
+
+    def get_sticker_price(self, stickers):
+        pass
+
+    def extract_charm_info(self, item_info):
+        pass
+
+    def get_charm_price(self, charm_name: str):
+        pass
+
+    def print_log(item: ItemData):
+        pass
+
+    def get_decision(self, item: ItemData):
+        """Функция определения выгодности лота"""
+        pass
 
 
 class SteamBot:
@@ -14,12 +57,17 @@ class SteamBot:
         session: ISteamSession,
         parser: Parser,
         itemInfoFetcher: MockItemInfoFetcher,
-        itemPriceFetcher: MockItemPriceFetcher,
+        itemPriceFetcher: IItemPriceFetcher,
     ):
         self.session = session
         self.parser = parser
         self.itemInfoFetcher = itemInfoFetcher
         self.itemPriceFetcher = itemPriceFetcher
+
+    def get_items_from_market(self, item_url):
+        raw_data = self.parser.get_raw_data_from_market(item_url)
+        json_data = self.parser.exract_json_from_raw_data(raw_data=raw_data)
+        return self.parser.extract_item_data(json_data)
 
     def start(self):
         if self.session.is_alive():
@@ -30,41 +78,39 @@ class SteamBot:
             items = self.get_items_from_market(item_url)
             self.process_items(item_name, items)
 
-    def get_items_from_market(self, item_url):
-        raw_data = self.parser.get_raw_data_from_market(item_url)
-        json_data = self.parser.exract_json_from_raw_data(raw_data=raw_data)
-        return self.parser.extract_item_data(json_data)
-
-    def get_item_info(self, inspect_link):
-        item_info = self.get_sticker_and_charm_info(inspect_link)
-        stickers = self.extract_sticker_info(item_info)
-        for sticker in stickers:
-            sticker["price"] = self.itemPriceFetcher.get_price_by_name(
-                sticker.get("name"))
-
-        stickers_price = self.get_sticker_price(stickers)
-
-        charm = self.extract_charm_info(item_info)
-        charm_price = self.get_charm_price(charm)
-        return stickers, stickers_price, charm, charm_price
-
     def process_items(self, item_name, items):
         for item in items:
             listing_id = item.get("listing_id")
             price = item.get("price")
             inspect_link = item.get("inspect_link")
             item_obj = ItemData(item_name, listing_id, inspect_link,
-                                price, *self.get_item_info(inspect_link))
-            self.get_decision(item_obj)
+                                price, *self.get_sticker_and_charm_info(inspect_link))
+
+            decision = self.calculate_sticker_profitability(item_obj)
+            message = create_message(item)
+            print(message)
 
     def get_sticker_and_charm_info(self, inspect_link):
-        return self.itemInfoFetcher.get_sticker_and_charm_info(inspect_link=inspect_link)
+        item_info = self.itemInfoFetcher.get_sticker_and_charm_info(
+            inspect_link=inspect_link)
+        stickers = self.extract_sticker_info(item_info)
+        for sticker in stickers:
+            sticker["price"] = self.itemPriceFetcher.get_price_by_name(
+                sticker.get("name"))
+
+        stickers_price = self.get_stickers_sum_price(stickers)
+        charm = self.extract_charm_info(item_info)
+        charm_price = self.get_charm_price(charm)
+        return stickers, stickers_price, charm, charm_price
 
     def extract_sticker_info(self, item_info):
         return self.itemInfoFetcher.extract_sticker_info(item_info)
 
-    def get_sticker_price(self, stickers):
-        return self.itemPriceFetcher.get_stickers_price(stickers)
+    def get_stickers_sum_price(self, stickers: list[dict]):
+        result_price = 0
+        for sticker in stickers:
+            result_price += sticker['price']
+        return result_price
 
     def extract_charm_info(self, item_info):
         return self.itemInfoFetcher.extract_charm_info(item_info)
@@ -76,9 +122,11 @@ class SteamBot:
         print(
             f"Listing {item.listing_id}: Item Price: {item.item_price}, Stickers Price: {item.stickers_price}, Charm Price: {item.charm_price}")
 
-    def get_decision(self, item: ItemData):
-        """Функция определения выгодности лота"""
-        print("--------------------")
+    def calculate_sticker_profitability(self, item: ItemData):
+        """Функция определения выгодности лота. Общая стоимость стикеров / стоимость лота"""
+        sticker_profitability = item.stickers_price / item.item_price
+        return sticker_profitability
 
-        message = create_message(item)
-        print(message)
+    def get_decision(self, item: ItemData) -> bool:
+        """Принимает решаение исходя из фактора """
+        return False
--- assets/prices.py
+++ assets/prices.py
@@ -4,20 +4,16 @@ from typing import Protocol
 
 
 class IItemPriceFetcher(Protocol):
+    def __init__(db_repostiotory):
+        pass
+    
     def get_price_by_name(self, item_name: str) -> float:
         """Возвращает стоимость предмета по его названию"""
-        pass
-
-    def get_stickers_price(stickers: list[dict]) -> float:
-        """Возвращает стоимость всех стикеров на скине"""
         pass
 
 
 class MockItemPriceFetcher(IItemPriceFetcher):
     def get_price_by_name(self, item_name):
-        return 0
+        return 1
 
-    def get_stickers_price(self, stickers: list[dict]):
-        result_price = 0
-        for sticker in stickers:
-            result_price += self.get_price_by_name(sticker['name'])
+    
